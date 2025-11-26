081d07f4b0f8b9871a4596f4146d832a3aa568c6
Huzaifa <huzaifaansari71096@gmail.com>
2025-11-26 16:34:15 +0530

    create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import { Login } from './pages/Login'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast';
+import { Admin } from './pages/Admin';
+import { Employee } from './pages/Employee';
 
 export const App = () => {
   return (
     <>
-    
+     <Toaster position="top-center" />     
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/admin' element={<Admin />} />
+          <Route path='/emply' element={<Employee />} />
+          {/* <Route path='/Login' element={<Login />} /> */}
+          {/* <Route path="/Sign" element={<Signup />}/> */}
+        </Routes>
+     </BrowserRouter>
     </>
   )
 }
