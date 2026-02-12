5b654e2fcf7e6198b452995c7e8f5eacdd4f5c39
huzaifatrunetic <huzaifa.a@truneticinfotech.com>
2026-02-12 11:20:24 +0530

    show new task count on employee dashbord using localStorage data

--- src/Components/others/TaskCard.jsx
+++ src/Components/others/TaskCard.jsx
@@ -2,31 +2,41 @@ import React from 'react'
 
 const TaskCard = ({user}) => {
   // if(!user) return null;
-  console.log(user)
+  console.log("this is user", user)
+
+  const data = JSON.parse(localStorage.getItem('employee'))
+const emply = data.find(
+  (elem)=> user?.firstName == elem.firstName
+);
+
+console.log(emply)
+
   return (
     <>
-     <div className='flex mt-16 justify-between gap-4'>
+    
+    <div className='flex mt-16 justify-between gap-4'>
        <div className='bg-blue-600 w-[40%] px-7 py-10 rounded-lg'>
-        <h2 className='text-white text-2xl font-semibold'>{user?.taskCounts.newTask}</h2>
+        <h2 className='text-white text-2xl font-semibold'>{emply?.taskCounts.newTask}</h2>
         <h3 className='text-white text-2xl font-semibold'>New Task</h3>
        </div>
 
         <div className='bg-blue-600 w-[40%] px-7 py-10 rounded-lg'>
-        <h2 className='text-white text-2xl font-semibold'>{user?.taskCounts.completed}</h2>
+        <h2 className='text-white text-2xl font-semibold'>{emply?.taskCounts.completed}</h2>
         <h3 className='text-white text-2xl font-semibold'>completed Task</h3>
        </div>
 
         <div className='bg-blue-600 w-[40%] px-7 py-10 rounded-lg'>
-        <h2 className='text-white text-2xl font-semibold'>{user?.taskCounts.active}</h2>
+        <h2 className='text-white text-2xl font-semibold'>{emply?.taskCounts.active}</h2>
         <h3 className='text-white text-2xl font-semibold'>active Task</h3>
        </div>
 
         <div className='bg-blue-600 w-[40%] px-7 py-10 rounded-lg'>
-        <h2 className='text-white text-2xl font-semibold'>{user?.taskCounts.failed}</h2>
+        <h2 className='text-white text-2xl font-semibold'>{emply?.taskCounts.failed}</h2>
         <h3 className='text-white text-2xl font-semibold'>Failed Task</h3>
        </div>
 
      </div>
+    
     </>
   )
 }
